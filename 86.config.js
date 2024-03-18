983b45cf8690a434f171b77a611ae205094b3ed8
K.k <1142305247@qq.com>
2024-03-18 17:18:31 +0800

    en

--- 86.config.js
+++ 86.config.js
@@ -1,12 +1,12 @@
 var pc = {
-    "windows": "https://testflight.apple.com/join/4y0DyYVA",
-    "mac": "https://testflight.apple.com/join/4y0DyYVA"
+    "windows": "https://testflight.apple.com/join/4ulvXKhA",
+    "mac": "https://testflight.apple.com/join/4ulvXKhA"
 }
 
 var app = {
     "android": "http://api.866chat.com:8888/86chat.apk",
-    "ios_store": "https://testflight.apple.com/join/IwlSWOzt",
-    "ios_enterprise": "https://testflight.apple.com/join/IwlSWOzt",
-    "ios_jump": "https://testflight.apple.com/join/IwlSWOzt",
+    "ios_store": "https://testflight.apple.com/join/4ulvXKhA",
+    "ios_enterprise": "https://testflight.apple.com/join/4ulvXKhA",
+    "ios_jump": "https://testflight.apple.com/join/4ulvXKhA",
     "android_google_link": "http://api.866chat.com:8888/86chat.apk",
 }
